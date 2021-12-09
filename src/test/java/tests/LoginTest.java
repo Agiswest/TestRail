a98906ff9aedd383986d79cc5843e13ff2fbc38d
Max <nerdy.n@mail.ru>
2021-12-09 15:57:30 +0300

    Added api adapter for project

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,6 +1,9 @@
 package tests;
 
+import adapters.BaseAdapter;
+import adapters.ProjectAdapter;
 import com.github.javafaker.Faker;
+import models.*;
 import org.testng.annotations.Test;
 import pages.*;
 
@@ -8,17 +11,21 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void loginTestPositive() {
+        Project project = ProjectFactory.get();
+        project.setSuiteMode("1");
+        project.setId(projectAdapter.createProject(project).getId());
         LoginPage loginPage = new LoginPage();
         loginPage.open();
         loginPage.login(user, password);
         ProjectsPage projectsPage = new ProjectsPage();
-        projectsPage.openProjectByUrl("TestExampleProject");
+        projectsPage.openProjectByUrl(project.getName(), project.getId());
         TestSuitesAndCasesPage testSuitesAndCasesPage = new TestSuitesAndCasesPage();
         String sectionName = new Faker().name().username();
         testSuitesAndCasesPage
                 .open()
                 .addCaseSection(sectionName);
         testSuitesAndCasesPage.caseSectionIsCreated(sectionName);
+        projectAdapter.deleteProject(project.getId());
 
     }
 }
